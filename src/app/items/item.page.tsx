7addba2f21e52be9da51eb032710555f0fd54e04
mzsoba <owt4832@gmail.com>
2025-03-14 14:50:08 +0900

    feat/ ssg로 변환

--- src/app/items/item.page.tsx
+++ src/app/items/item.page.tsx
@@ -1,40 +1,23 @@
+import ItemCard from '@/components/ItemCard';
 import { fetchItemList } from '@/utils/serverApi';
-import { useQuery } from '@tanstack/react-query'
+
 import React from 'react'
 
-const ItemPage = () => {
-    const {
-        data:itemList,
-        isPending,
-        isError,
-    } = useQuery({
-        queryKey:["itemList"],
-        queryFn:fetchItemList,
-    });
+const ItemPage = async() => {
+   const itemList = await fetchItemList();
 
-    if(isPending){
-        return <div>loaidng...</div>
-    }
-
-    if(isError){
-        return<div>error occur...</div>
-    }
-
-
-    console.log(itemList);
-
-  return <>
-    <h1>아이템 목록 페이지</h1>
-    {/* {
-    itemList.map((e)=>{
-        return<>
-            <div key={}>
-
-            </div>
-        </>
-    })
-    } */}
-  </>
+  return (
+    <>
+      <h1>아이템 목록 페이지</h1>
+      <div className="p4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      {
+        itemList.map((e) => {
+          return <ItemCard item={e} key={e.name} />;
+        })
+      }
+      </div>
+    </>
+  )
 }
 
 export default ItemPage
--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ItemPage from './item.page'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const ItemComponentPage = async() => {
-    const queryClient = new QueryClient();
+    
 
   return (<>
     <div>ItemComponentPage</div>
-    <QueryClientProvider client={queryClient}>
+    
      <ItemPage />
-    </QueryClientProvider>
+    
     </>
   )
 }
