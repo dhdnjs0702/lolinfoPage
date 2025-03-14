af9d69238665b0fe04ff8934aee6e808ebd4d3d8
mzsoba <owt4832@gmail.com>
2025-03-14 11:04:15 +0900

    feat/ 챔피언 페이지에서 각 영웅들을 그리는 카드 컴포넌트 생성

---
+++ src/components/ChampionCard.tsx
@@ -0,0 +1,22 @@
+import { ChampionData } from '@/types/Champions'
+import Image from 'next/image'
+import React from 'react'
+
+const ChampionCard = ({champion}:ChampionData) => {
+  return (
+    <div className="card sm:max-w-sm">
+  <div className="card-body">
+    <Image 
+        src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.image.full}`} 
+        alt={champion.name}
+        width={120}
+        height={120}
+    />
+    <p className="mb-4">챔피언 명: {champion.name}</p>
+    <p className="mb-4">{champion.title}</p>
+  </div>
+</div>
+  )
+}
+
+export default ChampionCard
