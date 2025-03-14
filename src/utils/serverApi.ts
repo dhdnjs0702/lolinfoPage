5030f291864e6b308a884a73ee743d27ac4e6548
mzsoba <owt4832@gmail.com>
2025-03-14 11:03:08 +0900

    feat/ 각 페이지별 서버 액션 함수 생성

---
+++ src/utils/serverApi.ts
@@ -0,0 +1,31 @@
+import { ChampionData } from "@/types/Champions";
+import { ItemData } from "@/types/Items";
+
+export const fetchChampionList = async() => {
+    const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json")
+
+    const {data}:{data:ChampionData} = await res.json();
+
+    const champList = Object.values(data);
+
+    return champList;
+}
+
+export const fetchItemList = async() => {
+    const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json")
+
+    const {data}:{data:ItemData} = await res.json();
+
+    const ItemList = Object.values(data);
+
+    console.log(ItemList);
+
+    return ItemList;
+}
+
+export const fetchChampionDetail = async(id:string)=>{
+    const res = await fetch(`https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion/${id}.json`);
+    const data = await res.json();
+    
+    return data.data;
+}
