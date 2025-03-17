d0cc884f814489c623ba9cd4f164e1780865dff6
mzsoba <owt4832@gmail.com>
2025-03-17 16:21:44 +0900

    feat/ 렌더링 패턴이 달라서 충돌이 발생했던 부분 오류 해결(ssr패턴 함수를 추가)

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -4,7 +4,7 @@ import { ItemData } from "@/types/Items";
 export const fetchChampionList = async() => {
     const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json",{
         next:{
-            revalidate: 86400,
+            revalidate: 86400, //isr
         }
     })
 
@@ -12,25 +12,39 @@ export const fetchChampionList = async() => {
 
     const champList = Object.values(data);
 
+    
+    return champList;
+}
+
+export const fetchChampionListSSR = async() => {
+    const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json",{
+        cache: "no-store"
+    })
+
+    const {data}:{data:ChampionData} = await res.json();
+
+    const champList = Object.values(data);
+
+    
     return champList;
 }
 
 export const fetchItemList = async() => {
     const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json",{
-        cache: "force-cache"
+        cache: "force-cache" //ssg
     })
 
     const {data}:{data:ItemData} = await res.json();
 
     const ItemList = Object.values(data);
 
-    console.log(ItemList);
-
     return ItemList;
 }
 
 export const fetchChampionDetail = async(id:string)=>{
-    const res = await fetch(`https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion/${id}.json`);
+    const res = await fetch(`https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion/${id}.json`,{
+        cache: "no-store" //ssr
+    });
     const data = await res.json();
     
     return data.data;
