0a1b1fee04fd17e085921968e642251015ddeccf
mzsoba <owt4832@gmail.com>
2025-03-18 15:05:22 +0900

    feat/ 아이템 페이지 타입 에러 문제 해결(promise가 반환되는것을 간과 하고 있었음)

--- src/app/items/item.page.tsx
+++ src/app/items/item.page.tsx
@@ -1,25 +1,24 @@
 import ItemCard from '@/components/ItemCard';
 import { fetchItemList } from '@/utils/serverApi';
+import React from 'react';
 
-import React from 'react'
-
-const ItemPage = async() => {
-   const itemList = await fetchItemList();
+const ItemPage = async () => {
+  const itemList = await fetchItemList();
 
   return (
     <>
-     <div className="flex items-center justify-center">
-    <h1 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">아이템 정보</h1>
-    </div>
+      <div className="flex items-center justify-center">
+        <h1 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">아이템 정보</h1>
+      </div>
       <div className="p4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-      {
-        itemList.map((e) => {
-          return <ItemCard item={e} key={e.name} />;
-        })
-      }
+        {
+          itemList.map((e) => {
+            return <ItemCard key={e.name} item={e} />;
+          })
+        }
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ItemPage
+export default ItemPage;
--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -29,14 +29,15 @@ export const fetchChampionListSSR = async() => {
     return champList;
 }
 
-export const fetchItemList = async() => {
-    const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json",{
-        cache: "force-cache" //ssg
-    })
+export const fetchItemList = async(): Promise<ItemData[]> => {
+    const res = await fetch("https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json", {
+        cache: "force-cache" 
+    });
 
-    const {data}:{data:ItemData} = await res.json();
+    const result = await res.json();
+    const data: Record<string, ItemData> = result.data;
 
-    const ItemList = Object.values(data);
+    const ItemList: ItemData[] = Object.values(data);
 
     return ItemList;
 }
